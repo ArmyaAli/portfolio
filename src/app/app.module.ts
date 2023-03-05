58d1313c7ddc8d4e9b5935c42c61ef9558a35276
Ali Umar <ali.umar.work@gmail.com>
2023-03-05 18:05:57 -0500

    updated modules package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './matetials.module';
+import { MaterialModule } from './materials.module';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { AboutComponent } from './about/about.component';
-import { ToolbarComponent, MenuComponent, sharedMenuState} from './toolbar/toolbar.component';
+import { ToolbarComponent, MenuComponent, sharedMenuState } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
 import { CareerComponent } from './career/career.component';
 import { EducationComponent } from './education/education.component';
 import { ResumeComponent } from './resume/resume.component';
+import { darkLightPipe } from './dark.light.pipe';
+import { stateService } from './state.service';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,16 +27,22 @@ import { ResumeComponent } from './resume/resume.component';
     ContactComponent,
     CareerComponent,
     EducationComponent,
-    ResumeComponent 
+    ResumeComponent,
+    darkLightPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    MatExpansionModule
+    MatExpansionModule,
+    PdfViewerModule,
+    ReactiveFormsModule
   ],
-  providers: [sharedMenuState],
+  providers: [
+    sharedMenuState, 
+    stateService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
