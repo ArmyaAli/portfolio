649feb339d43dba5fcffba31da5b81dd3c1e81df
Ali Umar <ali.umar.work@gmail.com>
2025-06-17 11:49:52 -0400

    Add missing domain names to domains list

--- src/data/domains.ts
+++ src/data/domains.ts
@@ -1,5 +1,8 @@
 export const domains = [
     { url: "aliumar.dev", active: true },
     { url: "lordmushroom.dev", active: false },
-    { url: "shroom.meme", active: false }
+    { url: "shroom.meme", active: false },
+    { url: "fucktrump.mom", active: false },
+    { url: "openforms.ca", active: false },
+    { url: "aliumar.net", active: false }
 ]
