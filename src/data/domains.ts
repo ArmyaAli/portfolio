4e28b1df19b706d573746b1e78189d7fdc348929
Ali Umar <ali.umar.work@gmail.com>
2026-03-05 22:07:29 +0000

    Upgrade Astro and streamline portfolio navigation

--- src/data/domains.ts
+++ src/data/domains.ts
@@ -2,7 +2,6 @@ export const domains = [
     { url: "aliumar.dev", active: true },
     { url: "lordmushroom.dev", active: false },
     { url: "shroom.meme", active: false },
-    { url: "fucktrump.mom", active: false },
     { url: "openforms.ca", active: false },
     { url: "aliumar.net", active: false }
 ]
